44f2edfa84cbdcbd9b5c2a449897031592cbf674
Python-Woman <everydayCodeStory@gmail.com>
2019-09-25 18:31:05 +0530

    A-RISE-OF-PYTHON : PYTHON CRASH COURSE
    -- Exercise 2.2 from completed

--- READING-LIST/Python Crash Course/CH-2/_2_Simple_Message.py
+++ READING-LIST/Python Crash Course/CH-2/_2_Simple_Message.py
@@ -1,2 +1,10 @@
-name = 'saumya tiwari is a GOOD Girl'
-print(name.title())
+# PYTHON CRASH COURSE : CH-2 by SAUMYA TIWARI
+#
+# 2-2. Simple Messages: Store a message in a variable, and print that message.
+# Then change the value of your variable to a new message, and print the new
+# message.
+
+message = "Hey Python, Welcome !!"
+print(message)
+message = "Tell Me what's New in this release."
+print(message)
